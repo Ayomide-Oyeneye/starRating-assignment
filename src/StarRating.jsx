5be79f1a8d96f1dff590ac5d8df5d202fc4f680a
Ayomide-Oyeneye <nickbelson11@gmail.com>
2023-09-08 16:36:50 -0700

    1 file modified

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 function StarRating({ rating }) {
   // Create an array to store the star elements based on the 'rating' prop
+
   const stars = [];
 
-  for (let i = 0; i < rating; i++) {
-    stars.push(
-      <img
-        key={i}
-        alt=" "
-        className="gold star"
-        src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
-      />
-    );
-  }
+Array.from({ length: rating }).forEach((_, i) => {
+  stars.push(
+    <img
+      key={i}
+      alt=" "
+      className="gold star"
+      src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+    />
+  );
+});
 
   return <div className="star-wrapper">{stars}</div>;
 }
